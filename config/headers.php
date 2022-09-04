d005fdcd8e69e6a53064a64560e287893fa4cad7
yassineghorbal <yassineghorbal2000@gmail.com>
2022-09-04 23:14:04 +0100

    changed headers for create.php

---
+++ config/headers.php
@@ -0,0 +1,13 @@
+<?php
+// required headers
+header('Access-Control-Allow-Origin: *');
+header('Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept, Authorization');
+header("Access-Control-Allow-Credentials: true");
+header('Content-Type: application/json');
+$method = $_SERVER['REQUEST_METHOD'];
+if ($method == "OPTIONS") {
+    header('Access-Control-Allow-Origin: *');
+    header("Access-Control-Allow-Headers: X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Request-Method,Access-Control-Request-Headers, Authorization");
+    header("HTTP/1.1 200 OK");
+    die();
+}
